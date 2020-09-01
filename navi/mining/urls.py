66e3670a435a9c20fd1291724e3d2d3da341f5eb
Zang <aaron.zang@intel.com>
2020-09-01 17:34:49 +0800

    'updated'

--- navi/mining/urls.py
+++ navi/mining/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mining import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.TestView.as_view(), name='index'),
 ]
